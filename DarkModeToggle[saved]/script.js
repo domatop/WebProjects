06ed489cfb2bcdac229d1a3f0e6a5fe3f69c59cb
Domagoj <67262063+domatop@users.noreply.github.com>
2022-06-14 22:37:55 +0200

    Add files via upload

---
+++ DarkModeToggle[saved]/script.js
@@ -0,0 +1,21 @@
+const body = document.querySelector("body")
+const InitalTheme = "light"
+
+const setTheme = (theme) => {
+    localStorage.setItem("theme", theme)
+    body.setAttribute("data-theme", theme)
+}
+const toggleTheme = () => {
+    const activeTheme = localStorage.getItem("theme");
+
+    if(activeTheme === "light") setTheme("dark");
+    else setTheme("light");
+}
+
+const setThemeOnInit = () => {
+    const savedTheme = localStorage.getItem("theme");
+    savedTheme
+        ? body.setAttribute("data-theme", savedTheme)
+        :setTheme(InitalTheme);
+}
+setThemeOnInit()
